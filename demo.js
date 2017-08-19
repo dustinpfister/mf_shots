c9e973bfcf4102104936ea77a6aa7be446c60eca
dustinpfister <dustin.pfister@gmail.com>
2017-08-19 17:49:16 -0400

    first commit

---
+++ demo.js
@@ -0,0 +1,117 @@
+
+var player = _.a({
+        x : 160,
+        y : 120
+    }),
+
+keys = [];
+
+window.onkeydown = function (e) {
+
+    console.log(e.keyCode)
+
+    keys[e.keyCode] = true;
+
+};
+
+window.onkeyup = function (e) {
+
+    keys[e.keyCode] = false;
+
+};
+
+_.a({
+    x : 5,
+    y : 5,
+    n : 'e'
+});
+
+(function () {
+
+    // create and inject a canvas
+    var canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+
+    setup = function () {
+
+        // append to body
+        document.body.appendChild(canvas);
+
+        // set actual matrix size of the canvas
+        canvas.width = 320;
+        canvas.height = 240;
+
+        loop();
+    },
+
+    // the single draw function
+    draw = function () {
+
+        // draw a cirlce
+        ctx.strokeStyle = '#ffffff';
+
+        _.s.u.forEach(function (u) {
+
+            ctx.fillStyle = u.n === 'p' ? '#00ff00' : '#ff0000';
+            ctx.fillRect(u.x, u.y, u.w, u.h);
+
+        });
+
+        ctx.stroke();
+
+    },
+
+    // clear screen
+    cls = function () {
+
+        // default the canvas to a solid back background
+        ctx.fillStyle = '#000000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    },
+
+    // the loop
+    loop = function () {
+
+        requestAnimationFrame(loop);
+
+        // update
+        _.u();
+
+        // w
+        if (keys[87]) {
+
+            player.b = 1;
+
+        }
+
+        //s
+        if (keys[83]) {
+
+            player.b = 0;
+
+        }
+
+        //a
+        if (keys[65]) {
+
+            player.a += Math.PI / 50;
+
+        }
+
+        //d
+        if (keys[68]) {
+
+            player.a -= Math.PI / 50;
+
+        }
+
+        cls();
+        draw();
+
+    };
+
+    setup();
+
+}
+    ());
---
+++ shots.js
@@ -0,0 +1,113 @@
+// a micro framework for space shooter type games
+
+var _ = (function () {
+
+    // U id for Unit Class
+    var U = function (o) {
+
+        o = o || {};
+        this.x = o.x || 0; // x pos
+        this.y = o.y || 0; // y pos
+
+        //this.a = o.a || 0; // delta x
+        //this.b = o.b || 0; // delta y
+
+        this.a = o.a || 0; // heading
+        this.b = o.b || 0; // speed
+
+        this.w = o.w || 8; // width
+        this.h = o.h || 8; // height
+
+    },
+
+    // S is for Shot Class
+    S = function (o) {
+
+        U.call(this, o);
+
+        this.l = o.l || 10; // life span
+
+    },
+
+    // V is for vessel
+    V = function (o) {
+
+        U.call(this, o);
+
+        this.n = o.n || 'p'; // n if for owner
+        this.H = 100; // max HP
+        this.i = this.H; // HP
+        this.shots = [];
+
+    };
+
+    // a if for API
+    a = {
+
+        // s id for state object
+        s : {
+
+            u : [], // the units array
+
+        },
+
+        // distance formula
+        d : function (x1, y1, x2, y2) {
+
+            return Math.sqrt(Math.abs(Math.pow(x1 - x2, 2)) + Math.abs(Math.pow(y1 - y2, 2)));
+
+        },
+
+        // add a new vessel
+        a : function (o) {
+
+            var v = new V(o);
+
+            this.s.u.push(v);
+
+            // return a reference
+            return v;
+
+        },
+
+        // update all units
+        u : function () {
+
+            this.s.u.forEach(function (u) {
+
+                // move by current deltas
+                //u.x += u.a;
+                //u.y += u.b;
+
+                u.x += Math.cos(u.a) * u.b;
+                u.y += Math.sin(u.a) * u.b;
+
+            });
+
+        }
+
+    };
+
+    // U.a(unitObj) - The angle to the given unit object
+    U.prototype.u = function (u) {
+
+        return this.p(u.x, u.y);
+
+    };
+
+    // angle to given Point
+    U.prototype.p = function (x, y) {
+
+        return Math.atan2(this.y - y, this.x - x);
+
+    };
+
+    // shot inherits from Unit
+    S.prototype = new U();
+
+    V.prototype = new U();
+
+    return a;
+
+}
+    ());
